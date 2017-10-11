8a375f948b5590143f7676ac72b3058b7ecae5bc
moepda <moepda@gmail.com>
2017-10-10 23:37:57 -0400

    Dont build gapps anymore
    -Not needed now gapps are working correctly
    
    Change-Id: I8d830b94eee5b357398d131d63156260afad04e5
    Signed-off-by: moepda <moepda@gmail.com>

--- device.mk
+++ device.mk
@@ -526,4 +526,4 @@ PRODUCT_PROPERTY_OVERRIDES += \
 ro.sys.sdcardfs=false
 
 # Build gapps for now
-$(call inherit-product-if-exists, vendor/shamugapps/shamu-gapps.mk)
+#$(call inherit-product-if-exists, vendor/shamugapps/shamu-gapps.mk)
